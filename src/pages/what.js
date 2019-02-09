3a83b72baa4cf122be3ee95442f343681d98b39d
Sean Preston <seanmpreston@gmail.com>
2019-02-09 11:26:42 +0000

    remove footer

--- src/pages/what.js
+++ src/pages/what.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 
 import heroText from '../media/images/hero-text.png';
 import hero from '../media/images/hero.png';
@@ -10,7 +10,6 @@ const WhatPage = () => (
     <p>are getting married</p>
     <img src={hero} />
     <h2>20th JULY 2019</h2>
-    <Footer></Footer>
   </div>
 );
 
