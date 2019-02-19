2e34139cf25855bb6009767cf714195f163cba5f
Sean Preston <seanmpreston@gmail.com>
2019-02-19 21:38:56 +0000

    gifts page

--- src/pages/gifts.js
+++ src/pages/gifts.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import gift from '../media/images/gift.png';
+
 const GiftsPage = () => (
   <div>
-    <h1>Buy us some shit</h1>
-    <p>thanks</p>
+    <h1>GIFT REGISTRY</h1>
+    <img src={gift} alt='a gift' />
+    <p>Of course, it is your presence not your presents that
+will make our day so special, however any cash
+contributions to our honeymoon in Mexico would be
+warmly accepted.</p>
+    <p>NB. very expensive gifts are also welcome.</p>
   </div>
 );
 
